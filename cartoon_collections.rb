0d86bafbbb9ee4a9921313fe47cb16ca2ff5d955
Matt Zuzolo <mattzuzolo@outlook.com>
2018-04-26 03:13:28 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,9 @@ def long_planeteer_calls (array_calls)
   end  
 end
 
+
+#snacks = ["crackers", "gouda", "thyme"]
+
 def find_the_cheese (array_strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
   
